d4fceb4a82ead5d9c654a7f38f13cb7517404bdd
Symphony <55611595+lnance6@users.noreply.github.com>
2020-09-07 20:47:53 -0700

    Refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,72 @@
-//display Current Day
+//Display date in header
 var today=moment().format("dddd, MMMM Do");
 var now =new Date();
 document.getElementById("currentDay").innerHTML=today;
+var colorCode = function() {
+  if (moment().isAfter(displayDate,'hour')) {
+    $(`#schItem${i}`).addClass("past");
+  } else if (moment().isBefore(displayDate,'hour')) {
+    $(`#schItem${i}`).addClass("future");
+  }
+    else $(`#schItem${i}`).addClass("present");
+    
+}
+//build grid
+for (var i=9; i<24; i++){
+  var curDay = new Date();
+  var calDate = new Date(curDay.getFullYear()+'-'+(curDay.getMonth()+1)+'-'+curDay.getDate() + " " + i +":00");
+  var dateCalHour=calDate.getHours();
+  var displayDate=moment(calDate,"hA");
+  var displayHour=moment(displayDate).format("hA");
+  var curHour=moment().format("hA");
+  console.log(curHour);
+  console.log(displayHour);
+  console.log(moment());
+  console.log(displayDate);
+  $('<div>',{id:`b${i}`}).addClass("row").appendTo($("div.container"))
+  $('<div>',{id:`hb${i}`}).addClass("col-1 hour").appendTo($(`#b${+i}`))
+  $('<span>',{id:`timeSpan${i}`}).addClass("time").text(displayHour).appendTo($(`#hb${i}`))
+  
+  $('<div>',{id:`db${i}`}).addClass("col-10 description").appendTo($(`#b${i}`))
+  $('<textArea>',{id:`schItem${i}`}).addClass("overflow-hidden").appendTo($(`#db${i}`))
+  colorCode();
+  $('<div>',{id:`sb${i}`}).addClass("col-1 saveBtn").appendTo($(`#b${i}`))
+  $('<span>',{id:`saveSpan${i}`}).addClass("align-middle").appendTo($(`#sb${i}`))
+  $('<i>',{id:`saveIcon${i}`}).addClass("fas fa-save").appendTo($(`#saveSpan${i}`))
+}
+
+
+
+ 
+ /*
+  $('<textarea>').each(function() {
+    if (dateCalHour<curHour) {
+      $(this).addClass("past");
+    } 
+    else if (dateCalHour>curHour) {
+      $(this).addClass("future");
+    }
+    else if (dateCalHour===curHour) {
+      $(this).addClass("present");
+    }
+  };*/
+
+/*
+var timeEl = document.querySelector(".hour");
+
+var createEvents = function(eventText,eventTime) {
+  // create elements that make up a task item
+  // append span and p element to parent li
+  taskLi.append(taskSpan, taskP);
+
+  // check due date
+  auditTask(taskLi);
+
+  // append to ul list on the page
+  $("#list-" + taskList).append(taskLi);
+};
+
+//display Current Day
 
 
 var curHour=moment().format("hA");
@@ -42,31 +107,58 @@ $('.hour').each(function(index,obj) {
       $(this).removeClass("past");
       $(this).removeClass("future");
     }
- 
+    var setTime=timeEl.setAttribute("textContent",curHour);
+    console.log(setTime)
   });
+
+});
+
+var loadEvents = function() {
+  events = JSON.parse(localStorage.getItem("events"));
+
+  // if nothing in localStorage, create a new object to track all task status arrays
+  if (!events) {
+    events = []
+  }
+  // loop over object properties
+  $.each(events, function(arr) {
+    // then loop over sub-array
+    arr.forEach(function(events) {
+      createEvents(event.text, event.time);
+    });
+  });
+};
+
+var saveEvents = function() {
+  localStorage.setItem("events", JSON.stringify(events));
+};
+
+// save button in modal was clicked
+$(".saveBtn").click(function(calHour) {
+  // get form values
+  var eventText = $("textarea").val();
+  var eventTime = calHour;
+
+  if (eventText && eventTime) {
+    createEvents(eventText, eventTime);
+
+    
+    // save in tasks array
+    events.toDo.push({
+      text: eventText,
+      date: eventTime
+    });
+
+    saveEvents();
+  }
 });
 
 
+$("div").on("click", "i", function() {
+  console.log("<i> was clicked");
+  saveEvents();
+});
+*/
 
-//var calHour=$(".hour").find("span").text().trim();
-//console.log(calHour)
 
-var auditTask = function(taskEl) {
-  // get date from task element
-  var date = $(taskEl).find("span").text().trim();
 
-  // convert to moment object at 5:00pm
-  var time = moment(date, "L").set("hour", 17);
-
-  // remove any old classes from element
-  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
-
-  // apply new class if task is near/over due date
-  if (moment().isAfter(time)) {
-    $(taskEl).addClass("list-group-item-danger");
-  } 
-  else if (Math.abs(moment().diff(time, "days")) <= 2) {
-    $(taskEl).addClass("list-group-item-warning");
-  }
-  console.log(taskEl)
-};
