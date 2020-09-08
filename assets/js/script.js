9f6b5c3b8e5537a6811866b1af74974d9c8b43c5
Symphony <55611595+lnance6@users.noreply.github.com>
2020-09-08 01:34:30 -0700

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var sItems = [];
+
 
 //Display date in header
 var today=moment().format("dddd, MMMM Do");
@@ -16,81 +16,61 @@ var colorCode = function() {
     
 }
 //build time block grid
-for (var i=9; i<24; i++){
+for (var i=0; i<9; i++){
+   var calcHours=i+9;
   var curDay = new Date();
-  var calDate = new Date(curDay.getFullYear()+'-'+(curDay.getMonth()+1)+'-'+curDay.getDate() + " " + i +":00");
+  var calDate = new Date(curDay.getFullYear()+'-'+(curDay.getMonth()+1)+'-'+curDay.getDate() + " " + calcHours +":00");
   var dateCalHour=calDate.getHours();
   var displayDate=moment(calDate,"hA");
   var displayHour=moment(displayDate).format("hA");
   var curHour=moment().format("hA");
- // console.log(curHour);
- // console.log(displayHour);
- // console.log(moment());
- // console.log(displayDate);
+ 
   $('<div>',{id:`b${i}`}).addClass("row").appendTo($("div.container"))
   $('<div>',{id:`hb${i}`}).addClass("col-1 hour").appendTo($(`#b${+i}`))
   $('<span>',{id:`timeSpan${i}`}).addClass("time").text(displayHour).appendTo($(`#hb${i}`))
   
-  $('<div>',{id:`db${i}`}).addClass("col-10 description").appendTo($(`#b${i}`))
-  $('<textArea>',{id:`schItem${i}`}).addClass("overflow-hidden").appendTo($(`#db${i}`))
+  $('<div>',{id:`db${i}`}).addClass("col-10 ").appendTo($(`#b${i}`))
+  $('<textArea>',{id:`schItem${i}`}).attr("name","item-name").addClass("description overflow-hidden").appendTo($(`#db${i}`))
   colorCode();
   $('<div>',{id:`sb${i}`}).addClass("col-1 saveBtn").appendTo($(`#b${i}`))
   $('<span>',{id:`saveSpan${i}`}).addClass("align-middle").appendTo($(`#sb${i}`))
   $('<i>',{id:`saveIcon${i}`}).addClass("fas fa-save").appendTo($(`#saveSpan${i}`))
 }
 
-$("div").on("click", "i", function() {
-  console.log("<i> was clicked");
-  //saveItems();
-});
+var list = JSON.parse(localStorage.getItem('todolist')) || [];
 
-
- /*
-
-
-var loadEvents = function() {
-  events = JSON.parse(localStorage.getItem("events"));
-
-  // if nothing in localStorage, create a new object to track all task status arrays
-  if (!events) {
-    events = []
+// Renders our to-dos to the page
+function renderTodos(list) {
+    //console.log(list);
+  // Iterates over the 'list'
+  for (var i = 0; i < list.length; i++) {
+    // Creates a new variable 'toDoItem' that will hold a "<textArea>" tag
+    // Sets the `list` item's value as text of this <textArea> element
+    var toDoItem = $('<textArea>');
+    toDoItem.text(list[i]);
+    //console.log(toDoItem);
+    // Adds 'toDoItem' to the To-Do List div
+    $('.description').append(toDoItem); 
   }
-  // loop over object properties
-  $.each(events, function(arr) {
-    // then loop over sub-array
-    arr.forEach(function(events) {
-      createEvents(event.text, event.time);
-    });
-  });
-};
+}
 
-var saveItems = function() {
-  localStorage.setItem("events", JSON.stringify(events));
-};
-
-// save button in modal was clicked
-$(".saveBtn").click(function(calHour) {
-  // get form values
-  var eventText = $("textarea").val();
-  var eventTime = calHour;
-
-  if (eventText && eventTime) {
-    createEvents(eventText, eventTime);
-
+$('span').on('click','i', function(event) {
     
-    // save in tasks array
-    sItems.push({
-      text: eventText,
-      date: eventTime
-    });
-
-    saveItems();
-  }
+// Get the to-do "value" from the textbox and store it as a variable using `.val()` and `.trim()`
+ 
+  var toDoValue= $('textArea').val().trim();
+  //console.log(toDoValue);
+  // Add the new to-do to our local 'list' variable
+  
+  list.push(toDoValue);
+  // Update the to-dos on the page
+  
+  renderTodos(list);
+  // Save the to-dos into localStorage
+  // We need to use JSON.stringify to turn the list from an array into a string
+  
+  localStorage.setItem('toDoList', JSON.stringify(list));
 });
 
-
-
-*/
-
-
-
+  // Renders our to-dos on page load
+  renderTodos(list);
